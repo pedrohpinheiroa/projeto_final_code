c89f00ea2ac10dd817bdf899bb6ce820d8f59e05
Pero henrique Pinheiro <pedrohpinheiro@poli.ufrj.br>
2025-05-03 11:28:20 -0300

    Implement new metrics system

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,3 +1,4 @@
+import numpy as np
 from .metrics import Metrics
 from .writer import Writer
 
@@ -11,50 +12,64 @@ class Logger:
 
     def reset(self):
         self.episode_data = {
-            'reward':[],
-            'critic_loss':[],
-            'q_values':[],
-            'actor_loss':[],
-            'actor_gradient':[],
-            'noise':[],
-            'position':[],
-            'velocity':[],
+            'reward':np.array([]),
+            'critic_loss':np.array([]),
+            'critic_gradient':np.array([]),
+            'predict_q':np.array([]),
+            'target_q':np.array([]),
+            'actor_loss':np.array([]),
+            'actor_gradient':np.array([]),
+            'noise':np.array([]),
+            'position':np.array([]),
+            'velocity':np.array([]),
         }
+        for key in self.episode_data:
+            self.episode_data[key] = np.array(self.episode_data[key], dtype=float)
 
-    def add_information(self, reward, critic_loss, q_value, actor_loss, actor_gradient, noise, position, velocity):
-        self.episode_data['reward'].append(reward)
-        self.episode_data['critic_loss'].append(critic_loss)
-        self.episode_data['q_values'].append(q_value)
-        self.episode_data['actor_loss'].append(actor_loss)
-        self.episode_data['actor_gradient'].append(actor_gradient)
-        self.episode_data['noise'].append(sum(noise) / len(noise))
-        self.episode_data['position'].append(position)
-        self.episode_data['velocity'].append(velocity)
+    def add_information(self, reward, critic_loss, critic_gradient, predict_q, target_q, actor_loss, actor_gradient, noise, position, velocity):
+        self.episode_data['reward'] = np.append(self.episode_data['reward'], reward)
+        self.episode_data['critic_loss'] = np.append(self.episode_data['critic_loss'], critic_loss)
+        self.episode_data['critic_gradient'] = np.append(self.episode_data['critic_gradient'], critic_gradient)
+        self.episode_data['predict_q'] = np.append(self.episode_data['predict_q'], predict_q)
+        self.episode_data['target_q'] = np.append(self.episode_data['target_q'], target_q)
+        self.episode_data['actor_loss'] = np.append(self.episode_data['actor_loss'], actor_loss)
+        self.episode_data['actor_gradient'] = np.append(self.episode_data['actor_gradient'], actor_gradient)
+        self.episode_data['noise'] = np.append(self.episode_data['noise'], noise)
+        self.episode_data['position'] = np.append(self.episode_data['position'], position)
+        self.episode_data['velocity'] = np.append(self.episode_data['velocity'], velocity)
     
     def save(self, episode, real_episode_duration, virtual_episode_duration):
-        self.writer.write_scalar('Episode Duration (Virtual)', virtual_episode_duration, episode)
-        self.writer.write_scalar('Episode Duration (Real)', real_episode_duration, episode)
+        self.writer.write_scalar('Episode/Duration (Virtual)', virtual_episode_duration, episode)
+        self.writer.write_scalar('Episode/Duration (Real)', real_episode_duration, episode)
 
         mean_reward = self.metrics.mean_reward(self.episode_data)
-        self.writer.write_scalar('Mean Reward', mean_reward, episode)
-
         best_mean_reward = self.metrics.best_mean_reward(mean_reward)
-        self.writer.write_scalar('Best Mean Reward', best_mean_reward, episode)
+        self.writer.write_scalar('Reward/Mean', mean_reward, episode)
+        self.writer.write_scalar('Reward/Best Mean', best_mean_reward, episode)
 
-        critic_loss, actor_loss = self.metrics.loss(self.episode_data)
-        self.writer.write_scalar('Total Critic Loss', critic_loss, episode)
-        self.writer.write_scalar('Total Actor Loss', actor_loss, episode)
-        self.writer.write_scalar('Total Loss', critic_loss + actor_loss, episode)
+        mean_critic_loss = self.metrics.mean_critic_loss(self.episode_data)
+        mean_critic_gradient = self.metrics.mean_critic_gradient(self.episode_data)
+        mean_predict_q = self.metrics.mean_predict_q(self.episode_data)
+        mean_target_q = self.metrics.mean_target_q(self.episode_data)
+        self.writer.write_scalar('Critic/Mean Loss', mean_critic_loss, episode)
+        self.writer.write_scalar('Critic/Mean Gradient', mean_critic_gradient, episode)
+        self.writer.write_scalar('Critic/Mean Predict Q-value', mean_predict_q, episode)
+        self.writer.write_scalar('Critic/Mean Target Q-value', mean_target_q, episode)
+
+        mean_actor_loss = self.metrics.mean_actor_loss(self.episode_data)
+        mean_actor_gradient = self.metrics.mean_actor_gradient(self.episode_data)
+        self.writer.write_scalar('Actor/Mean Loss', mean_actor_loss, episode)
+        self.writer.write_scalar('Actor/Mean Gradient', mean_actor_gradient, episode)
 
         noise = self.metrics.noise(self.episode_data)
-        self.writer.write_scalar('Mean Noise', noise, episode)
+        self.writer.write_scalar('Noise/Mean', noise, episode)
 
         mean_position = self.metrics.mean_position(self.episode_data)
-        self.writer.write_scalar('Mean Position', mean_position, episode)
         mode_position = self.metrics.mode_position(self.episode_data)
-        self.writer.write_scalar('Mode Position', mode_position, episode)
+        self.writer.write_scalar('Position/Mean', mean_position, episode)
+        self.writer.write_scalar('Position/Mode', mode_position, episode)
 
         mean_velocity = self.metrics.mean_velocity(self.episode_data)
-        self.writer.write_scalar('Mean Velocity', mean_velocity, episode)
         mode_velocity = self.metrics.mode_velocity(self.episode_data)
-        self.writer.write_scalar('Mode Velocity', mode_velocity, episode)
+        self.writer.write_scalar('Velocity/Mean', mean_velocity, episode)
+        self.writer.write_scalar('Velocity/Mode', mode_velocity, episode)
--- src/logger/metrics.py
+++ src/logger/metrics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Metrics:
 
     def __init__(self):
@@ -5,16 +7,11 @@ class Metrics:
 
     def mean_reward(self, episode_data):
         episode_rewards = episode_data['reward']
-        return sum(episode_rewards) / len(episode_rewards) if episode_rewards else 0
-
-    def loss(self, episode_data):
-        critic_loss = episode_data['critic_loss']
-        actor_loss = episode_data['actor_loss']
-        return sum(critic_loss), sum(actor_loss)
+        return np.mean(episode_rewards)
     
     def noise(self, episode_data):
         noise = episode_data['noise']
-        return sum(noise) / len(noise) if noise else 0
+        return  np.mean(noise)
 
     def best_mean_reward(self, mean_reward):
         if mean_reward > self.training_best_reward:
@@ -23,17 +20,44 @@ class Metrics:
     
     def mean_position(self, episode_data):
         position = episode_data['position']
-        return sum(position) / len(position)
+        return  np.mean(position)
     
     def mode_position(self, episode_data):
         position = episode_data['position']
+        position = position.tolist()
         return max(set(position), key=position.count)
     
     def mean_velocity(self, episode_data):
         velocity = episode_data['velocity']
-        return sum(velocity) / len(velocity)
+        return np.mean(velocity)
     
     def mode_velocity(self, episode_data):
         velocity = episode_data['velocity']
+        velocity = velocity.tolist()
         return max(set(velocity), key=velocity.count)
+
+    def mean_critic_loss(self, episode_data):
+        critic_loss = episode_data['critic_loss']
+        critic_loss = np.array(critic_loss)
+        return np.mean(critic_loss.flatten())
     
+    def mean_critic_gradient(self, episode_data):
+        critic_gradient = episode_data['critic_gradient']
+        return np.mean(critic_gradient)
+    
+    def mean_predict_q(self, episode_data):
+        q_values = episode_data['predict_q']
+        return np.mean(q_values)
+    
+    def mean_target_q(self, episode_data):
+        target_q = episode_data['target_q']
+        target_q = np.array(target_q)
+        return np.mean(target_q.flatten())
+
+    def mean_actor_loss(self, episode_data):
+        actor_loss = episode_data['actor_loss']
+        return np.mean(actor_loss.flatten())
+
+    def mean_actor_gradient(self, episode_data):
+        actor_gradient = episode_data['actor_gradient']
+        return np.mean(actor_gradient)

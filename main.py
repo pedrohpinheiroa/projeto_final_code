36ce4670258b463ceaf968784c870600209bfee8
Pero henrique Pinheiro <pedrohpinheiro@poli.ufrj.br>
2025-05-03 11:28:37 -0300

    implement new metrics system

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-# Preciso tratar o tempo em Miliseconds. Isso vai reduzir o erro matemático dos binários.
-# Talvez isso corrija o bug da duração máxima do episódio ser 8 segundos, com tempos de execução tão divergentes.
-import os
-import json
 import time
 from src.environment.virtual_enviroment import Seesaw
 from src.agent.DDPG import Agent
 from src.logger import Logger
 
-def create_checkpoint(agent:Agent):
-    DIR = "./checkpoint"
-    agent.actor.model.save_weights(f"{DIR}/actor_model.weights.h5")
-    agent.actor.target_model.save_weights(f"{DIR}/actor_target.weights.h5")
-    agent.critic.model.save_weights(f"{DIR}/critic_model.weights.h5")
-    agent.critic.target_model.save_weights(f"{DIR}/critic_target.weights.h5")
-
-def load_checkpoint(agent:Agent):
-    DIR = "./checkpoint"
-    try:
-        agent.actor.model.load_weights(f"{DIR}/actor_model.weights.h5")
-        agent.actor.target_model.load_weights(f"{DIR}/actor_target.weights.h5")
-        agent.critic.model.load_weights(f"{DIR}/critic_model.weights.h5")
-        agent.critic.target_model.load_weights(f"{DIR}/critic_target.weights.h5")
-    except:
-        pass
-    finally:
-        return agent
-
 
 env = Seesaw()
 agent = Agent()
 logger = Logger()
-agent = load_checkpoint(agent)
 
-EPISODES = 100_000
+EPISODES = 10_000
 for episode in range(EPISODES):
     episode_start = time.time()
     env.reset()
@@ -44,14 +20,16 @@ for episode in range(EPISODES):
         reward = env.get_reward()
         next_state = env.get_state()
         done = env.is_done()
-        critic_loss, q_values, actor_loss, actor_gradient = agent.learn()
+        critic_loss, critic_gradient, predict_q, target_q, actor_loss, actor_gradient = agent.learn()
         
         env.save_in_history(state)
         agent.add_experience(state, action, reward, next_state, done)
         logger.add_information(
             reward=reward,
             critic_loss=critic_loss,
-            q_value=q_values,
+            critic_gradient=critic_gradient,
+            predict_q=predict_q,
+            target_q=target_q,
             actor_loss=actor_loss,
             actor_gradient=actor_gradient,
             noise=noise,
@@ -69,7 +47,5 @@ for episode in range(EPISODES):
     logger.reset()
     if episode % 50 == 0 and episode > 0:
         agent.save()
-    
-    create_checkpoint(agent)
-    
+        
     print(f"Episode {episode} finished. Time: {time.time() - episode_start:.2f}s. Total Virtual Time: {state['time']/1000:.4f}s Final Position: {state['position']:.4f}", end=("\n"))

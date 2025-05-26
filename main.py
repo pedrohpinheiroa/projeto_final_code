43c8dceda373f7219caf3151b6da9d15dcccec58
Pero henrique Pinheiro <pedrohpinheiro@poli.ufrj.br>
2025-05-26 13:05:23 -0300

    refactor main file

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 import time
+
+import tensorflow as tf
 from src.environment.virtual_enviroment import Seesaw
 from src.agent.DDPG import Agent
 from src.logger import Logger
 
+def main():
+    env = Seesaw()
+    agent = Agent()
+    logger = Logger()
 
-env = Seesaw()
-agent = Agent()
-logger = Logger()
-
-EPISODES = 10_000
-for episode in range(EPISODES):
-    episode_start = time.time()
-    env.reset()
-    
-    while not env.is_done():
-        state = env.get_state()
-        action, noise = agent.act(state)
-        env.step(action)
-        reward = env.get_reward()
-        next_state = env.get_state()
-        done = env.is_done()
-        critic_loss, critic_gradient, predict_q, target_q, actor_loss, actor_gradient = agent.learn()
+    EPISODES = 10_000
+    for episode in range(EPISODES):
+        episode_start = time.time()
+        env.reset()
         
+        while not env.is_done():
+            state = env.get_state()
+            action, noise = agent.act(state)
+            env.step(action)
+            reward = env.get_reward()
+            next_state = env.get_state()
+            done = env.is_done()
+            critic_loss, critic_gradient, predict_q, target_q, actor_loss, actor_gradient = agent.learn()
+            
+            env.save_in_history(state)
+            agent.add_experience(state, action, reward, next_state, done)
+            logger.add_information(
+                reward=reward,
+                critic_loss=critic_loss,
+                critic_gradient=critic_gradient,
+                predict_q=predict_q,
+                target_q=target_q,
+                actor_loss=actor_loss,
+                actor_gradient=actor_gradient,
+                noise=noise,
+                position = state['position'],
+                velocity = state['velocity'],
+            )
+
+        state = env.get_state()
         env.save_in_history(state)
-        agent.add_experience(state, action, reward, next_state, done)
-        logger.add_information(
-            reward=reward,
-            critic_loss=critic_loss,
-            critic_gradient=critic_gradient,
-            predict_q=predict_q,
-            target_q=target_q,
-            actor_loss=actor_loss,
-            actor_gradient=actor_gradient,
-            noise=noise,
-            position = state['position'],
-            velocity = state['velocity'],
+        logger.save(
+            episode=episode, 
+            real_episode_duration = time.time() - episode_start,
+            virtual_episode_duration = next_state['time'],
         )
+        logger.reset()
+        agent.decay_noise()
+        
+        print(f"Episode {episode} finished. Time: {time.time() - episode_start:.2f}s. Total Virtual Time: {state['time']/1000:.4f}s Final Position: {state['position']:.4f}", end=("\n"))
 
-    state = env.get_state()
-    env.save_in_history(state)
-    logger.save(
-        episode=episode, 
-        real_episode_duration = time.time() - episode_start,
-        virtual_episode_duration = next_state['time'],
-    )
-    logger.reset()
-    if episode % 1000 == 0 and episode > 0:
-        agent.noise_decay()
-    
-    print(f"Episode {episode} finished. Time: {time.time() - episode_start:.2f}s. Total Virtual Time: {state['time']/1000:.4f}s Final Position: {state['position']:.4f}", end=("\n"))
+    agent.save()
 
-agent.save()
+if __name__ == "__main__":
+    with tf.device('/CPU:0'):
+        main()

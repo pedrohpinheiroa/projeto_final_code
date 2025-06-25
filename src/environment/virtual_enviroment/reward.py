98a9cbc3e4f5f98acb6ca13c403208f8c1176636
Pero henrique Pinheiro <pedrohpinheiro@poli.ufrj.br>
2025-06-25 17:07:13 -0300

    implement new reward system

--- src/environment/virtual_enviroment/reward.py
+++ src/environment/virtual_enviroment/reward.py
@@ -16,6 +16,7 @@ class Reward:
     def set_configs(self):
         self.position_weight = self.configs['position_weight']
         self.velocity_weight = self.configs['velocity_weight']
+        self.torque_weight = self.configs['torque_weight']
         self.knock_penalty = self.configs['knock_penalty']
         self.over_pwm_penalty = self.configs['over_pwm_penalty']
         self.under_pwm_penalty = self.configs['under_pwm_penalty']
@@ -23,37 +24,35 @@ class Reward:
         self.stability_threshold = self.configs['stability_threshold']
 
     def position_reward(self, state: Dict) -> float:
-        position_reward = np.abs(state['position']) / self.max_angle
-        position_reward = np.round(position_reward, 3)
-        position_reward = np.clip(position_reward, 0.0, 1.0)
-        position_reward = -position_reward ** 2
+        normalized_position = state['position'] / self.max_angle
+        abs_normalized_position = np.abs(normalized_position)
+        position_reward = -1 + np.cos(abs_normalized_position * np.pi / 2)
         reward = self.position_weight * position_reward
-        reward = np.round(reward, 3)
         return reward
     
     def velocity_reward(self, state: Dict) -> float:
-        velocity_penalty = -abs(state['velocity']) ** 2
-        reward = self.velocity_weight * velocity_penalty
+        normalized_position = state['position'] / self.max_angle
+
+        velocity = np.clip(state['velocity'],-1, 1)
+        cos_factor = (normalized_position/ self.max_angle) + velocity
+        cos_factor = abs(cos_factor)
+        velocity_reward = -1 + np.cos(cos_factor * np.pi / 2)
+        reward = self.velocity_weight * velocity_reward
         return reward
     
+    def torque_reward(self, state: Dict) -> float:
+        torque_penalty = -abs(state['torque']) ** 2
+        reward = self.torque_weight * torque_penalty
+        return reward
+        
     def check_knock(self, state: Dict) -> float:
         if state['knock']:
             return self.knock_penalty
         return 0.0
 
-    def check_over_pwm(self, state: Dict) -> float:
-        if state['over_pwm']:
-            return self.over_pwm_penalty
-        return 0.0
-
-    def check_under_pwm(self, state: Dict) -> float:
-        if state['under_pwm']:
-            return self.under_pwm_penalty
-        return 0.0
-
     def check_stability(self, state: Dict) -> float:
-        if (abs(state['position']) < self.stability_threshold and 
-            abs(state['velocity']) < self.stability_threshold):
+        if (abs(state['position']) <= self.stability_threshold and 
+            abs(state['velocity']) <= self.stability_threshold):
             return self.stability_reward
         return 0.0
 
@@ -61,8 +60,8 @@ class Reward:
         reward = 0.0
         reward += self.position_reward(state)
         reward += self.velocity_reward(state)
-        reward += self.check_knock(state)
+
         reward += self.check_stability(state)
-        # reward += self.check_over_pwm(state)
-        # reward += self.check_under_pwm(state)
+        reward += self.check_knock(state)
+
         return reward

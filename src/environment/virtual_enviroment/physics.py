810e8e584d6db8cc172309ee678beeb5e046630d
Pero henrique Pinheiro <pedrohpinheiro@poli.ufrj.br>
2025-06-25 22:45:45 -0300

    change action input method

--- src/environment/virtual_enviroment/physics.py
+++ src/environment/virtual_enviroment/physics.py
@@ -6,15 +6,15 @@ import numpy as np
 
 class Physics:
 
-    def __init__(self):
+    def __init__(self, min_pwm, max_pwm):
         self.read_configs()
-        self.set_configs()
+        self.set_configs(min_pwm, max_pwm)
 
     def read_configs(self):
         with open('configs/physics.json', 'r') as file:
             self.configs = json.load(file)
 
-    def set_configs(self):
+    def set_configs(self, min_pwm, max_pwm):
         self.gravity = self.configs['gravity']
         self.time_step = self.configs['time_step']
 
@@ -25,39 +25,60 @@ class Physics:
         self.inertia = (self.bar_mass * (self.bar_length ** 2) / 3)
         self.inertia += (2 * self.motor_mass * (self.bar_length ** 2))
 
+        self.min_pwm = min_pwm
+        self.max_pwm = max_pwm
+        self.pwm_range = self.max_pwm - self.min_pwm
+
+    def _get_pwms(self, action: float) -> Tuple[float, float]:
+        action = action[0]
+        if action < 0:
+            right_pwm = self.min_pwm
+            left_pwm = self.min_pwm + abs(action) * self.pwm_range
+        elif action > 0:
+            left_pwm = self.min_pwm
+            right_pwm = self.min_pwm + abs(action) * self.pwm_range
+        else:
+            left_pwm = self.min_pwm
+            right_pwm = self.min_pwm
+        
+        left_pwm = np.clip(left_pwm, self.min_pwm, self.max_pwm)
+        right_pwm = np.clip(right_pwm, self.min_pwm, self.max_pwm)
+        return np.float16(left_pwm), np.float16(right_pwm)
+
     def _get_forces(self, left_pwm: float, right_pwm: float)->Tuple[float, float]:
         left_force = self.gravity*(70*left_pwm - 1)/100
         right_force = self.gravity*(70*right_pwm - 1)/100
         return left_force, right_force
     
-    def _get_torques(self, left_force: float, right_force: float)->Tuple[float, float]:
+    def _get_torque(self, left_force: float, right_force: float)->Tuple[float, float]:
         left_torque = left_force * self.bar_length
         right_torque = right_force * self.bar_length
-        return left_torque, right_torque
-    
-    def _get_acceleration(self, left_torque: float, right_torque: float)->float:
         torque = right_torque - left_torque
-        return np.round(torque / self.inertia,1)
+        return torque
+    
+    def _get_acceleration(self, torque: float)->float:
+        return torque / self.inertia
     
     def _get_velocity(self, acceleration: float, velocity: float)->float:
         time_step = self.time_step/1000
-        return np.round(velocity + acceleration * time_step, 3)
+        return velocity + acceleration * time_step
     
     def _get_position(self, velocity: float, position: float)->float:
         time_step = self.time_step/1000
-        return np.round(position + velocity * time_step, 4)
+        return position + velocity * time_step
     
-    def apply_action(self, state:Dict, action: Tuple[float, float]):
-        left_pwm, right_pwm = action
+    def apply_action(self, state:Dict, action: float):
+        left_pwm, right_pwm = self._get_pwms(action)
         left_force, right_force = self._get_forces(left_pwm, right_pwm)
-        left_torque, right_torque = self._get_torques(left_force, right_force)
-        acceleration = self._get_acceleration(left_torque, right_torque)
+        torque = self._get_torque(left_force, right_force)
+        acceleration = self._get_acceleration(torque)
         velocity = self._get_velocity(acceleration, state['velocity'])
         position = self._get_position(velocity, state['position'])
         state['time'] = state['time'] + self.time_step
         state['position'] = position
         state['velocity'] = velocity
         state['acceleration'] = acceleration
+        state['torque'] = torque
         state['left_pwm'] = np.float16(left_pwm)
         state['right_pwm'] = np.float16(right_pwm)
         return state
